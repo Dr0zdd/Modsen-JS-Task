d40aad32dac3f6a6e9e72f73f3771733784a12d4
Ilya Kantysh <ikantysh05@gmail.com>
2025-06-26 22:03:17 +0300

    refactor: change structure of the project

--- task/src/js/search.js
+++ task/src/js/search.js
@@ -1,7 +1,6 @@
 import { getApiImages, renderImages } from './gallery.js';
 
 const searchInput = document.getElementById('searchInput');
-const hintsContainer = document.getElementById('searchHints');
 const allowedCharsRegex = /^[a-zA-Z0-9!$&*\-=_^`|~#%'+/?_{}]+$/;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,28 +27,21 @@ async function handleSearch() {
 
 async function handleSearchHints() {
     const searchValue = searchInput?.value.trim().toLowerCase();
-    if (!searchValue || searchValue.length < 3 || !allowedCharsRegex.test(searchValue)) return;
 
-    const apiImages = await getApiImages();
-    hintsContainer.innerHTML = '';
+    if (!searchValue || searchValue.length < 3 || !allowedCharsRegex.test(searchValue)) {
+        searchInput.classList.add('invalid');
+        return;
+    }
 
-    const matchingHints = apiImages
-        .map(({ alt_description }) => alt_description?.toLowerCase())
-        .filter(desc => desc?.includes(searchValue))
-        .slice(0, 5);
-
-    hintsContainer.innerHTML = matchingHints.length ? `<p>Совпадения: ${matchingHints.join(', ')}</p>` : '';
+    searchInput.classList.remove('invalid');
 }
 
 
 function filterImages(images, searchValue) {
-    return images.filter(({ alt_description, id }, index) =>
-        (alt_description || '').toLowerCase().includes(searchValue) ||
-        String(id) === searchValue ||
-        String(index + 1) === searchValue
+    return images.filter(({ alt_description }) =>
+        (alt_description || '').toLowerCase().includes(searchValue)
     );
 }
-
 
 async function logImageDetails() {
     const apiImages = await getApiImages();
